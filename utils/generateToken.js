1addb8db3141338d7ee0f20d37de2cc71a2b5ccb
Mehran096 <mehranh91309@gmail.com>
2026-06-23 00:03:04 +0500

    new c ---

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -13,7 +13,8 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    sameSite: 'none',
+    //sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     maxAge: 30 * 24 * 60 * 60 * 1000,
     domain: 'localhost'
   })
